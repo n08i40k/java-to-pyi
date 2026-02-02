a68ced5089aef1121a07a431d23d91b983f48e51
n08i40k <nhomcencko1@gmail.com>
2026-02-02 16:18:48 +0400

    refactor: refactor ast preprocessing

--- src/index_table.rs
+++ src/index_table.rs
@@ -1,4 +1,5 @@
-use java_ast_parser::ast::ClassCell;
+use itertools::Itertools;
+use java_ast_parser::ast::{ClassCell, Root};
 use std::{collections::HashMap, rc::Rc};
 
 pub type IndexTable = HashMap<String, ClassCell>;
@@ -14,15 +15,18 @@ pub struct GlobalIndexTable(IndexTable);
 
 impl<'a> FromIterator<&'a QualifiedIndexTable> for GlobalIndexTable {
     fn from_iter<IT: IntoIterator<Item = &'a QualifiedIndexTable>>(iter: IT) -> Self {
-        let mut idt = IndexTable::new();
+        let mut index_table = IndexTable::new();
 
-        for i in iter {
-            for (k, v) in &i.idt {
-                idt.insert(format!("{}.{}", i.package, k), v.clone());
+        for qualified_table in iter {
+            for (key, class_cell) in &qualified_table.idt {
+                index_table.insert(
+                    format!("{}.{}", qualified_table.package, key),
+                    class_cell.clone(),
+                );
             }
         }
 
-        Self(idt)
+        Self(index_table)
     }
 }
 
@@ -42,6 +46,136 @@ impl From<IndexTable> for ImportedIndexTable {
 }
 
 impl ImportedIndexTable {
+    pub fn from_imports<'a, I>(
+        imports: &[impl AsRef<str>],
+        qualified_index_tables: I,
+    ) -> Self
+    where
+        I: IntoIterator<Item = &'a QualifiedIndexTable>,
+    {
+        let mut imported_table = IndexTable::new();
+
+        for qualified_table in qualified_index_tables {
+            let qualified_keys = qualified_table
+                .idt
+                .keys()
+                .map(|x| x.split('.').collect::<Box<[_]>>())
+                .collect::<Box<[_]>>();
+
+            for import_path in imports {
+                let import_path = import_path.as_ref();
+                let package = qualified_table.package.as_str();
+
+                if !import_path.starts_with(package) {
+                    continue;
+                }
+
+                let Some((_, import_suffix)) = import_path.split_at_checked(package.len() + 1)
+                else {
+                    continue;
+                };
+
+                if import_suffix == "*" {
+                    for (key, class_cell) in &qualified_table.idt {
+                        imported_table.insert(key.to_string(), class_cell.clone());
+                    }
+                    break;
+                }
+
+                for qualified_key in &qualified_keys {
+                    let mut segments = import_suffix.split('.').zip_longest(qualified_key.iter());
+
+                    let mut should_insert = false;
+                    let mut prev_ident: Option<&str> = None;
+
+                    while let Some(segment) = segments.next() {
+                        should_insert = false;
+
+                        match segment {
+                            itertools::EitherOrBoth::Both(import_segment, ident) => {
+                                if import_segment == "*" {
+                                    let remaining_idents = segments
+                                        .filter_map(|segment| match segment {
+                                            itertools::EitherOrBoth::Both(_, ident) => Some(ident),
+                                            itertools::EitherOrBoth::Left(_) => None,
+                                            itertools::EitherOrBoth::Right(ident) => Some(ident),
+                                        })
+                                        .join(".");
+
+                                    let import_key = if remaining_idents.is_empty() {
+                                        ident.to_string()
+                                    } else {
+                                        format!("{}.{}", ident, remaining_idents)
+                                    };
+
+                                    let class_cell = qualified_table
+                                        .idt
+                                        .get(&qualified_key.join("."))
+                                        .unwrap();
+
+                                    imported_table.insert(import_key, class_cell.clone());
+
+                                    break;
+                                }
+
+                                if import_segment != *ident {
+                                    break;
+                                }
+
+                                should_insert = true;
+                                prev_ident = Some(ident);
+                            }
+                            itertools::EitherOrBoth::Left(_) => break,
+                            itertools::EitherOrBoth::Right(ident) => {
+                                let Some(prev_ident) = prev_ident else {
+                                    break;
+                                };
+
+                                let remaining_idents = segments
+                                    .filter_map(|segment| match segment {
+                                        itertools::EitherOrBoth::Both(_, ident) => Some(ident),
+                                        itertools::EitherOrBoth::Left(_) => None,
+                                        itertools::EitherOrBoth::Right(ident) => Some(ident),
+                                    })
+                                    .join(".");
+
+                                let scoped_key = format!("{}.{}", prev_ident, ident);
+
+                                let import_key = if remaining_idents.is_empty() {
+                                    scoped_key
+                                } else {
+                                    format!("{}.{}", scoped_key, remaining_idents)
+                                };
+
+                                let class_cell = qualified_table
+                                    .idt
+                                    .get(&qualified_key.join("."))
+                                    .unwrap();
+
+                                imported_table.insert(import_key, class_cell.clone());
+
+                                break;
+                            }
+                        }
+                    }
+
+                    if should_insert {
+                        imported_table.insert(
+                            qualified_key.last().unwrap().to_string(),
+                            qualified_table
+                                .idt
+                                .get(&qualified_key.join("."))
+                                .unwrap()
+                                .clone(),
+                        );
+                    }
+                }
+            }
+        }
+
+        Self::from(imported_table)
+    }
+
     pub fn search(&self, query: &str) -> Option<ClassCell> {
         self.0.get(query).cloned()
     }
@@ -78,18 +212,19 @@ impl LocalIndexTable {
         self.local
             .idt
             .keys()
-            .find_map(|key| {
+            .find_map(|qualified_key| {
                 if let Some(scope) = scope
-                    && !key.starts_with(scope)
+                    && !qualified_key.starts_with(scope)
                 {
                     return None;
                 }
 
-                let (_, rem) = key.split_at_checked(scope.map(|x| x.len() + 1).unwrap_or(0))?;
+                let (_, remaining_key) =
+                    qualified_key.split_at_checked(scope.map(|x| x.len() + 1).unwrap_or(0))?;
 
                 // dbg!(&rem, &query);
-                if rem == query {
-                    self.local.idt.get(key).cloned()
+                if remaining_key == query {
+                    self.local.idt.get(qualified_key).cloned()
                 } else {
                     None
                 }
@@ -112,5 +247,40 @@ impl LocalIndexTable {
         self.search_local(scope, query)
             .or_else(|| self.search_imported(query))
             .or_else(|| self.search_global(query))
+    }
+}
+
+impl QualifiedIndexTable {
+    pub fn from_ast(ast: &Root) -> Self {
+        let mut index_table = IndexTable::new();
+
+        fn walk_class(
+            index_table: &mut IndexTable,
+            parent_path: Option<&str>,
+            class_cell: &ClassCell,
+        ) {
+            let class = class_cell.borrow();
+
+            let qualified_name = if let Some(parent_path) = parent_path {
+                format!("{}.{}", parent_path, class.ident)
+            } else {
+                class.ident.to_string()
+            };
+
+            for class in &class.classes {
+                walk_class(index_table, Some(&qualified_name), class);
+            }
+
+            index_table.insert(qualified_name, class_cell.clone());
+        }
+
+        for class in &ast.classes {
+            walk_class(&mut index_table, None, class);
+        }
+
+        Self {
+            package: ast.package.to_string(),
+            idt: index_table,
+        }
     }
 }
---
+++ src/preprocess.rs
@@ -0,0 +1,379 @@
+use std::{
+    collections::HashMap,
+    fs,
+    ops::{Deref, DerefMut},
+    path::Path,
+    rc::Rc,
+};
+
+use java_ast_parser::ast::{self, ClassCell, Modifiers};
+use topo_sort::{SortResults, TopoSort};
+
+use crate::index_table::{GlobalIndexTable, ImportedIndexTable, LocalIndexTable, QualifiedIndexTable};
+
+/// Parse ast and convert it to owned ("disconnect" from string).
+pub fn parse_java_ast<P: AsRef<Path>>(path: P) -> Option<ast::Root> {
+    let data = fs::read_to_string(path).unwrap();
+
+    let mut ast = match java_ast_parser::parse(&data) {
+        Ok(ast) => ast,
+        Err(e) => {
+            match &e {
+                lalrpop_util::ParseError::UnrecognizedToken { token, expected: _ } => {
+                    let line = data[..token.0].chars().filter(|&ch| ch == '\n').count() + 1;
+
+                    let column = token.0 - data[..token.0].rfind('\n').unwrap_or(0);
+
+                    let position = format!("line {}, column {}", line, column);
+                    println!("{}\n{}", e, position);
+                }
+                _ => {
+                    println!("{}", e);
+                }
+            };
+
+            return None;
+        }
+    };
+
+    strip_non_public_symbols(&mut ast);
+
+    Some(ast)
+}
+
+/// Remove `private`, `protected` and `static` functions and variables from all classes in ast
+/// recursively.
+/// As we don't see them in Python (afaik).
+fn strip_non_public_symbols(ast: &mut ast::Root) {
+    fn should_keep_variable(modifiers: &Modifiers) -> bool {
+        modifiers.intersects(Modifiers::PUBLIC)
+            && !modifiers.intersects(Modifiers::STATIC | Modifiers::FINAL)
+    }
+
+    fn should_keep_function(modifiers: &Modifiers) -> bool {
+        modifiers.intersects(Modifiers::PUBLIC)
+    }
+
+    fn walk_class(class: &mut java_ast_parser::ast::Class) {
+        class.variables = class
+            .variables
+            .clone()
+            .into_iter()
+            .filter(|v| should_keep_variable(&v.modifiers))
+            .collect();
+
+        class.functions = class
+            .functions
+            .clone()
+            .into_iter()
+            .filter(|f| should_keep_function(&f.modifiers))
+            .collect();
+
+        for class in class.classes.iter_mut() {
+            walk_class(class.borrow_mut().deref_mut());
+        }
+    }
+
+    for class in ast.classes.iter_mut() {
+        walk_class(class.borrow_mut().deref_mut());
+    }
+}
+
+/// ClassPtr -> Local Scope
+/// # Examples:
+/// ru.n08i40k.MyClass -> None
+/// ru.n08i40k.MyClass.InnerClass -> Some("MyClass")
+fn build_local_scope_map(ast: &ast::Root) -> HashMap<ClassCell, String> {
+    let mut scope_map: HashMap<ClassCell, String> = HashMap::new();
+
+    fn walk_class(
+        scope_map: &mut HashMap<ClassCell, String>,
+        scope: Option<&str>,
+        class_cell: &ClassCell,
+    ) {
+        let class = class_cell.borrow();
+
+        if let Some(scope) = &scope {
+            scope_map.insert(class_cell.clone(), scope.to_string());
+        }
+
+        let scope = if let Some(parent) = scope {
+            format!("{}.{}", parent, &class.ident)
+        } else {
+            class.ident.to_string()
+        };
+
+        for class_cell in &class.classes {
+            walk_class(scope_map, Some(&scope), class_cell);
+        }
+    }
+
+    for class_cell in &ast.classes {
+        walk_class(&mut scope_map, None, class_cell);
+    }
+
+    scope_map
+}
+
+fn resolve_type_name(
+    type_name: &mut ast::TypeName,
+    scope: Option<&str>,
+    local_index_table: &LocalIndexTable,
+) {
+    let ast::TypeName::Ident(ident) = type_name else {
+        return;
+    };
+
+    let Some(type_cell) = local_index_table.search(scope, ident) else {
+        return;
+    };
+
+    *type_name = ast::TypeName::ResolvedIdent(type_cell.clone());
+}
+
+/// ChildPtr -> ParentPtr
+fn resolve_type_names<'a, T: IntoIterator<Item = &'a (ClassCell, &'a LocalIndexTable)>>(
+    scope_map: &HashMap<ClassCell, String>,
+    classes_with_tables: T,
+) -> HashMap<ClassCell, ClassCell> {
+    let extends_map: HashMap<ClassCell, ClassCell> = HashMap::new();
+
+    for (class_cell, local_index_table) in classes_with_tables {
+        let scope = scope_map.get(class_cell).map(|x| x.as_str());
+
+        let mut class = class_cell.borrow_mut();
+
+        if let Some(extends) = &mut class.extends {
+            resolve_type_name(&mut extends.name, scope, local_index_table);
+        }
+
+        for variable in &mut class.variables {
+            resolve_type_name(&mut variable.r#type.name, scope, local_index_table);
+        }
+
+        for function in &mut class.functions {
+            resolve_type_name(&mut function.return_type.name, scope, local_index_table);
+        }
+    }
+
+    extends_map
+}
+
+fn collect_scoped_classes<'a, T: IntoIterator<Item = &'a Scope>>(
+    scopes: T,
+) -> Box<[(ClassCell, &'a LocalIndexTable)]> {
+    let mut classes: Vec<(ClassCell, &'a LocalIndexTable)> = Vec::new();
+
+    fn walk_class<'a>(
+        classes: &mut Vec<(ClassCell, &'a LocalIndexTable)>,
+        local_index_table: &'a LocalIndexTable,
+        class_cell: &ClassCell,
+    ) {
+        classes.push((class_cell.clone(), local_index_table));
+
+        for class in &class_cell.borrow().classes {
+            walk_class(classes, local_index_table, class);
+        }
+    }
+
+    for scope in scopes {
+        for class in &scope.ast.classes {
+            walk_class(&mut classes, &scope.local_index_table, class);
+        }
+    }
+
+    classes.into_boxed_slice()
+}
+
+fn merge_inherited_members<'a, T: IntoIterator<Item = &'a ClassCell>>(
+    classes_iter: T,
+    extends_map: &HashMap<ClassCell, ClassCell>,
+) {
+    for class_cell in classes_iter {
+        let mut class = class_cell.borrow_mut();
+
+        if class.extends.is_none() {
+            continue;
+        }
+
+        let Some(parent_cell) = extends_map.get(class_cell) else {
+            continue;
+        };
+
+        let parent = parent_cell.borrow();
+
+        class.variables = {
+            let mut variables = parent.variables.clone().into_vec();
+            variables.extend_from_slice(&class.variables);
+            variables.into_boxed_slice()
+        };
+
+        class.functions = {
+            let mut functions = parent.functions.clone().into_vec();
+            functions.extend_from_slice(&class.functions);
+            functions.into_boxed_slice()
+        };
+    }
+}
+
+fn strip_unknown_extends<'a, T: IntoIterator<Item = &'a ClassCell>>(
+    classes_iter: T,
+    _extends_map: &HashMap<ClassCell, ClassCell>,
+) {
+    for class_cell in classes_iter {
+        let mut class = class_cell.borrow_mut();
+
+        match class.extends {
+            None => continue,
+            Some(ast::Type {
+                name: ast::TypeName::ResolvedIdent(_),
+                ..
+            }) => continue,
+            _ => {}
+        }
+
+        class.extends = None;
+    }
+}
+
+fn topo_sort_extendables<'a, T: IntoIterator<Item = &'a ClassCell>>(
+    classes_iter: T,
+    classes_iter_len: usize,
+) -> Option<Box<[ClassCell]>> {
+    let mut topo_sort = TopoSort::with_capacity(classes_iter_len);
+
+    for class_cell in classes_iter {
+        if let Some(ast::Type {
+            name: ast::TypeName::ResolvedIdent(type_cell),
+            ..
+        }) = &class_cell.borrow().extends
+        {
+            topo_sort.insert(class_cell.clone(), vec![type_cell.clone()]);
+        }
+    }
+
+    let SortResults::Full(nodes) = topo_sort.into_vec_nodes() else {
+        return None;
+    };
+
+    Some(nodes.into_boxed_slice())
+}
+
+pub struct Scope {
+    pub ast: Rc<ast::Root>,
+    pub local_index_table: LocalIndexTable,
+}
+
+impl Scope {
+    pub fn from_roots(roots: &[Rc<ast::Root>]) -> Box<[Self]> {
+        let qualified_tables_by_root = {
+            let mut tables_by_root: HashMap<*const ast::Root, QualifiedIndexTable> =
+                HashMap::new();
+
+            for root in roots.iter() {
+                tables_by_root.insert(
+                    root.deref() as *const ast::Root,
+                    QualifiedIndexTable::from_ast(root),
+                );
+            }
+
+            tables_by_root
+        };
+
+        let qualified_tables = qualified_tables_by_root.values().collect::<Box<_>>();
+
+        let global_index_table = Rc::new(GlobalIndexTable::from_iter(
+            qualified_tables_by_root.values(),
+        ));
+
+        roots
+            .iter()
+            .map(|root| {
+                let imported_index_table = ImportedIndexTable::from_imports(
+                    root.imports
+                        .iter()
+                        .map(|x| x.as_str())
+                        .collect::<Box<[_]>>()
+                        .as_ref(),
+                    qualified_tables_by_root.values(),
+                );
+
+                let qualified_index_table = qualified_tables
+                    .iter()
+                    .filter_map(|v| {
+                        if v.package != root.package {
+                            return None;
+                        }
+
+                        Some(v.idt.clone())
+                    })
+                    .reduce(|left, right| {
+                        let mut merged = HashMap::new();
+
+                        merged.extend(left);
+                        merged.extend(right);
+
+                        merged
+                    })
+                    .map(|merged_table| QualifiedIndexTable {
+                        package: root.package.clone(),
+                        idt: merged_table,
+                    })
+                    .unwrap();
+
+                let local_index_table = LocalIndexTable::new(
+                    global_index_table.clone(),
+                    imported_index_table,
+                    qualified_index_table,
+                );
+
+                Scope {
+                    ast: root.clone(),
+                    local_index_table,
+                }
+            })
+            .collect::<Box<_>>()
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    CircullarDependency,
+}
+
+pub fn preprocess_asts(roots: &[Rc<ast::Root>], inherit_by_merge: bool) -> Result<(), Error> {
+    let scopes = Scope::from_roots(roots);
+
+    let classes_with_tables = collect_scoped_classes(&scopes);
+
+    let scope_map = roots
+        .iter()
+        .map(|x| build_local_scope_map(x.deref()))
+        .reduce(|left, right| {
+            let mut merged = HashMap::new();
+
+            merged.extend(left);
+            merged.extend(right);
+
+            merged
+        })
+        .unwrap_or(HashMap::new());
+
+    let extends_map = resolve_type_names(&scope_map, &classes_with_tables);
+
+    let classes = classes_with_tables
+        .into_iter()
+        .map(|(x, _)| x)
+        .collect::<Box<_>>();
+
+    strip_unknown_extends(&classes, &extends_map);
+
+    if inherit_by_merge {
+        let sorted_classes = topo_sort_extendables(&classes, classes.len())
+            .ok_or(Error::CircullarDependency)?;
+
+        merge_inherited_members(&sorted_classes, &extends_map);
+    }
+
+    Ok(())
+}
